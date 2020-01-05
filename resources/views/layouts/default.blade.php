9c81c033dd41dd59b4edfbf4532cfde2b20b62be
phper <2998658517@qq.com>
2020-01-05 06:59:49 +0000

    基础页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh_CN">
 <head>
   <meta charset="UTF-8">
-  <title>@yield('title','weibo App') - Laravel新手入门课程</title>
+  <title>@yield('title','weibo App') - Laravel新手入门课程 </title>
 </head>
 <body>
   @yield('content')
