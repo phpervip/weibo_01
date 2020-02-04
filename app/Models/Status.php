420b322f65a2eac4a368a717028faf7a8443134d
phper <2998658517@qq.com>
2020-02-04 12:37:39 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    //
+    protected $fillable = ['content'];
     public function user()
     {
         return $this->belongTo(User::class);
