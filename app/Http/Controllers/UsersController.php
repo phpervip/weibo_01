25b03b7ed70dcd9b4d7d27a2f28cb135d37abdca
phper <2998658517@qq.com>
2020-02-04 04:48:04 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store','index']
         ]);
 
         $this->middleware('guest', [
@@ -20,6 +20,12 @@ class UsersController extends Controller
         ]);
     }
 
+    public function index()
+    {
+        // $users = User::all();
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
     public function create()
     {
         return view('users.create');
